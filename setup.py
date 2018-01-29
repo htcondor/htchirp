e2c06c3a970d65ebec9973c917b6d0f4e6ceffab
Jason Patton <jpatton@cs.wisc.edu>
2018-01-29 15:46:19 -0600

    Updating setup.py

--- setup.py
+++ setup.py
@@ -1,13 +1,14 @@
 from setuptools import setup
 
-setup(name='htchirp',
-          version='0.1',
-          description='Pure Python Chirp client for HTCondor',
-          keywords='htcondor chirp',
-          url='https://github.com/jasoncpatton/htchirp',
-          author='Jason Patton',
-          author_email='jpatton@cs.wisc.edu',
-          license='ASL 2.0',
-          packages=['htchirp'],
-          zip_safe=False)
-          
+setup(
+    name='htchirp',
+    version='0.2',
+    description='Pure Python Chirp client for HTCondor',
+    keywords='htcondor chirp',
+    url='https://github.com/htcondor/htchirp',
+    author='Jason Patton',
+    author_email='jpatton@cs.wisc.edu',
+    license='ASL 2.0',
+    packages=['htchirp'],
+    zip_safe=False
+    )
