b9b0c631ed8d37aadebc190940466a6ae849ed32
Jason Patton <jpatton@cs.wisc.edu>
2023-11-22 10:59:31 -0600

    Increment version

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ with open(os.path.join(wd, 'README.md')) as f:
 
 setup(
     name='htchirp',
-    version='2.0',
+    version='3.0',
     description='Pure Python Chirp client for HTCondor',
     long_description=long_description,
     long_description_content_type='text/markdown',
